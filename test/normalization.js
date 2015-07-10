00058c1f6944708a09d1a48e8795bb5955c14e2f
Talles L <talleslasmar@gmail.com>
2015-07-10 13:06:58 -0300

    OCD

--- test/normalization.js
+++ test/normalization.js
@@ -5,67 +5,57 @@ var assert            = require('assert')
 
 it('should normalize a single string', function () {
 
-  assert.equal(normalize('eu sempre tive uma certeza'),
-                         'eu sempre tive uma certeza')
+  assert.equal(
+      normalize('eu sempre tive uma certeza')
+    ,           'eu sempre tive uma certeza')
 
-  assert.equal(normalize('que só me deu desilusão'),
-                         'que so me deu desilusao')
+  assert.equal(
+      normalize('que só me deu desilusão')
+    ,           'que so me deu desilusao')
 
-  assert.equal(normalize('é que o amor é uma tristeza'),
-                         'e que o amor e uma tristeza')
+  assert.equal(
+      normalize('é que o amor é uma tristeza')
+    ,           'e que o amor e uma tristeza')
 
-  assert.equal(normalize('muita mágoa demais para um coração'),
-                         'muita magoa demais para um coracao')
+  assert.equal(
+      normalize('muita mágoa demais para um coração')
+    ,           'muita magoa demais para um coracao')
 
 })
 
 it('should normalize an array of strings', function () {
 
-  assert.deepEqual(normalize([ 'eu', 'sempre', 'tive', 'uma', 'certeza' ]),
-                             [ 'eu', 'sempre', 'tive', 'uma', 'certeza' ])
+  assert.deepEqual(
+      normalize([ 'eu', 'sempre', 'tive', 'uma', 'certeza' ])
+    ,           [ 'eu', 'sempre', 'tive', 'uma', 'certeza' ])
 
-  assert.deepEqual(normalize([ 'que', 'só', 'me', 'deu', 'desilusão' ]),
-                         [ 'que', 'so', 'me', 'deu', 'desilusao' ])
+  assert.deepEqual(
+      normalize([ 'que', 'só', 'me', 'deu', 'desilusão' ])
+    ,           [ 'que', 'so', 'me', 'deu', 'desilusao' ])
 
-  assert.deepEqual(normalize([ 'é', 'que', 'o', 'amor', 'é', 'uma', 'tristeza' ]),
-                         [ 'e', 'que', 'o', 'amor', 'e', 'uma', 'tristeza' ])
+  assert.deepEqual(
+      normalize([ 'é', 'que', 'o', 'amor', 'é', 'uma', 'tristeza' ])
+    ,           [ 'e', 'que', 'o', 'amor', 'e', 'uma', 'tristeza' ])
 
-  assert.deepEqual(normalize(['muita', 'mágoa', 'demais', 'para', 'um', 'coração' ]),
-                         [ 'muita', 'magoa', 'demais', 'para', 'um', 'coracao' ])
+  assert.deepEqual(
+      normalize([ 'muita', 'mágoa', 'demais', 'para', 'um', 'coração' ])
+    ,           [ 'muita', 'magoa', 'demais', 'para', 'um', 'coracao' ])
 
 
 })
 
 it('should throw when given not a string nor array', function () {
 
-  assert.throws(
-      function () { normalize() }
-    , NotArrayNorString
-  )
-
-  assert.throws(
-    function () { normalize(null)}
-    , NotArrayNorString
-  )
-
-  assert.throws(
-    function () { normalize(123)}
-    , NotArrayNorString
-  )
-
-  assert.throws(
-    function () { normalize(/lol/)}
-    , NotArrayNorString
-  )
+  assert.throws(function () { normalize()      }, NotArrayNorString)
+  assert.throws(function () { normalize(null)  }, NotArrayNorString)
+  assert.throws(function () { normalize(123)   }, NotArrayNorString)
+  assert.throws(function () { normalize(/lol/) }, NotArrayNorString)
 
 })
 
 it('should throw when there\'s a non string in array', function () {
 
-  assert.throws(
-    function () { normalize([ '1', 2, '3' ])}
-    , NonStringInArray
-  )
+  assert.throws(function () { normalize([ '1', 2, '3' ])}, NonStringInArray)
 
 })
 
