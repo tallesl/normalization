ecb3ea4db35de10d3b07ffd0ded0a1430cb40e83
Talles L <talleslasmar@gmail.com>
2015-07-10 01:07:55 -0300

    supporting array of string

--- test/normalization.js
+++ test/normalization.js
@@ -1,17 +1,71 @@
-var assert    = require('assert')
-var normalize = require('../lib/normalization')
+var assert            = require('assert')
+  , normalize         = require('../lib/normalization')
+  , NotArrayNorString = require('../lib/errors/not-array-nor-string')
+  , NonStringInArray  = require('../lib/errors/non-string-in-array')
 
-it('should normalize correctly', function () {
-  assert.equal(normalize('Eu sempre tive uma certeza'),
+it('should normalize a single string', function () {
+
+  assert.equal(normalize('eu sempre tive uma certeza'),
                          'eu sempre tive uma certeza')
 
-  assert.equal(normalize('Que só me deu desilusão'),
+  assert.equal(normalize('que só me deu desilusão'),
                          'que so me deu desilusao')
 
-  assert.equal(normalize('É que o amor é uma tristeza'),
+  assert.equal(normalize('é que o amor é uma tristeza'),
                          'e que o amor e uma tristeza')
 
-  assert.equal(normalize('Muita mágoa demais para um coração'),
+  assert.equal(normalize('muita mágoa demais para um coração'),
                          'muita magoa demais para um coracao')
+
+})
+
+it('should normalize an array of strings', function () {
+
+  assert.deepEqual(normalize([ 'eu', 'sempre', 'tive', 'uma', 'certeza' ]),
+                             [ 'eu', 'sempre', 'tive', 'uma', 'certeza' ])
+
+  assert.deepEqual(normalize([ 'que', 'só', 'me', 'deu', 'desilusão' ]),
+                         [ 'que', 'so', 'me', 'deu', 'desilusao' ])
+
+  assert.deepEqual(normalize([ 'é', 'que', 'o', 'amor', 'é', 'uma', 'tristeza' ]),
+                         [ 'e', 'que', 'o', 'amor', 'e', 'uma', 'tristeza' ])
+
+  assert.deepEqual(normalize(['muita', 'mágoa', 'demais', 'para', 'um', 'coração' ]),
+                         [ 'muita', 'magoa', 'demais', 'para', 'um', 'coracao' ])
+
+
+})
+
+it('should throw when given not a string nor array', function () {
+
+  assert.throws(
+      function () { normalize() }
+    , NotArrayNorString
+  )
+
+  assert.throws(
+    function () { normalize(null)}
+    , NotArrayNorString
+  )
+
+  assert.throws(
+    function () { normalize(123)}
+    , NotArrayNorString
+  )
+
+  assert.throws(
+    function () { normalize(/lol/)}
+    , NotArrayNorString
+  )
+
+})
+
+it('should throw when there\'s a non string in array', function () {
+
+  assert.throws(
+    function () { normalize([ '1', 2, '3' ])}
+    , NonStringInArray
+  )
+
 })
 
